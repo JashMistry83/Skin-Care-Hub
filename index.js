feb230d70f5bd96218a92b9f6592ac00b329b94a
JashMistry83 <jashmistry1721@gmail.com>
2025-08-19 18:00:45 +0530

    Ui inhencement...3

--- index.js
+++ index.js
@@ -37,98 +37,99 @@ app.post("/getuv", async (req, res) => {
   //Area name that entered by user
   const AreaName = req.body.AreaName;
 
-  if(AreaName.length !== 1){
-    
-  //get the cordinates of that area name by using location cordinates API & created a JS object
-  const cordinates = await axios.get(
-    `${LocationBaseURL}?q=${AreaName}&key=${LocationApiKey}`
-  );
+  if (AreaName.length !== 1) {
+    //get the cordinates of that area name by using location cordinates API & created a JS object
+    const cordinates = await axios.get(
+      `${LocationBaseURL}?q=${AreaName}&key=${LocationApiKey}`
+    );
 
-  //our API response is like that in our "cordinates" object we got "data" & in that we got "results" array in that we got "components" this is where all cords get stored
-  if (cordinates.data.results[0]) {
-    const ActualCityName = cordinates.data.results[0].components.city;
-    const ActualCountryName = cordinates.data.results[0].components.country;
-    const ActualCityDistrictName = cordinates.data.results[0].components.state_district;
-    const ActualContinent = cordinates.data.results[0].components.continent;
-    const ActualStateName = cordinates.data.results[0].components.state;
-    // console.log(ActualAreaName);
-    console.log(cordinates.data.results);
-    const lat = cordinates.data.results[0].geometry.lat;
-    const lon = cordinates.data.results[0].geometry.lng;
+    //our API response is like that in our "cordinates" object we got "data" & in that we got "results" array in that we got "components" this is where all cords get stored
+    if (cordinates.data.results[0]) {
+      const ActualCityName = cordinates.data.results[0].components.city;
+      const ActualCountryName = cordinates.data.results[0].components.country;
+      const ActualCityDistrictName =
+        cordinates.data.results[0].components.state_district;
+      const ActualContinent = cordinates.data.results[0].components.continent;
+      const ActualStateName = cordinates.data.results[0].components.state;
+      // console.log(ActualAreaName);
+      console.log(cordinates.data.results);
+      const lat = cordinates.data.results[0].geometry.lat;
+      const lon = cordinates.data.results[0].geometry.lng;
 
-    const result = await axios.get(`${BaseURL}?lat=${lat}&lng=${lon}`, {
-      headers: { "x-access-token": ApiKEY },
-    });
+      const result = await axios.get(`${BaseURL}?lat=${lat}&lng=${lon}`, {
+        headers: { "x-access-token": ApiKEY },
+      });
 
-    let UV = result.data.result.uv; //this is the UV of specified location of current time
-    let UVmax = result.data.result.uv_max; //this is maximum uv of that specified location on that day
-    let UVmaxTime = result.data.result.uv_max_time; //this is maximum uv's time of that specified location on that day
-    let date = new Date(UVmaxTime); //this is the date object that in differ time format
+      let UV = result.data.result.uv; //this is the UV of specified location of current time
+      let UVmax = result.data.result.uv_max; //this is maximum uv of that specified location on that day
+      let UVmaxTime = result.data.result.uv_max_time; //this is maximum uv's time of that specified location on that day
+      let date = new Date(UVmaxTime); //this is the date object that in differ time format
 
-    //this is the advices variables in string values
-    let Night = "It's night buddy go outside openly.";
-    let low =
-      "Minimal risk. No protection needed unless you have very sensitive skin.";
-    let moderate =
-      "Moderate risk. Wear sunglasses and consider SPF 30+ sunscreen if outside for more than 30 minutes.";
-    let high =
-      "High UV exposure. Use SPF 30+ sunscreen, sunglasses, and a hat. Seek shade between 10 AM and 4 PM.";
-    let veryhigh =
-      "Very high risk of skin damage. Apply SPF 50+ sunscreen, wear protective clothing, and avoid midday sun.";
-    let extreme =
-      "Extreme danger from UV rays! Stay indoors during peak hours. If outdoors, reapply SPF 50+ every 2 hours and cover all exposed skin.";
+      //this is the advices variables in string values
+      let Night = "It's night buddy go outside openly.";
+      let low =
+        "Minimal risk. No protection needed unless you have very sensitive skin.";
+      let moderate =
+        "Moderate risk. Wear sunglasses and consider SPF 30+ sunscreen if outside for more than 30 minutes.";
+      let high =
+        "High UV exposure. Use SPF 30+ sunscreen, sunglasses, and a hat. Seek shade between 10 AM and 4 PM.";
+      let veryhigh =
+        "Very high risk of skin damage. Apply SPF 50+ sunscreen, wear protective clothing, and avoid midday sun.";
+      let extreme =
+        "Extreme danger from UV rays! Stay indoors during peak hours. If outdoors, reapply SPF 50+ every 2 hours and cover all exposed skin.";
 
-    let advice;
+      let advice;
 
-    if (UV == 0) {
-      advice = Night;
-    } else if (UV < 3) {
-      advice = low;
-    } else if (UV > 2 || UV < 6) {
-      advice = moderate;
-    } else if (UV > 5 || UV < 8) {
-      advice = high;
-    } else if (UV > 7 || UV < 11) {
-      advice = veryhigh;
-    } else if (UV > 10) {
-      advice = extreme;
+      if (UV == 0) {
+        advice = Night;
+      } else if (UV < 3) {
+        advice = low;
+      } else if (UV > 2 || UV < 6) {
+        advice = moderate;
+      } else if (UV > 5 || UV < 8) {
+        advice = high;
+      } else if (UV > 7 || UV < 11) {
+        advice = veryhigh;
+      } else if (UV > 10) {
+        advice = extreme;
+      }
+
+      //that "date" object that we convert that in IST time format
+      const IstTime = date.toLocaleString("en-IN", {
+        timeZone: "Asia/Kolkata",
+        hour12: true, // 12-hour format
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      });
+
+     
+      console.log(result.data);
+      //   console.log("MAX-UV :",result.uv_max);
+      //   console.log("MAX-UV TIME :",result.uv_max_time);
+      res.render("index.ejs", {
+        content: result.data,
+        Time: IstTime,
+        advice,
+        ActualCityName,
+        ActualCountryName,
+        ActualStateName,
+        ActualCityDistrictName,
+        ActualContinent,
+        //this error have to be null otherwise it will remains old values
+        error: null,
+      });
+    } else {
+      res.render("index.ejs", {
+        error: "This location's data is not available...",
+        //this content have to be null otherwise it will remains old values
+        content: null,
+      });
     }
-
-    //that "date" object that we convert that in IST time format
-    const IstTime = date.toLocaleString("en-IN", {
-      timeZone: "Asia/Kolkata",
-      hour12: true, // 12-hour format
-      day: "2-digit",
-      month: "short",
-      year: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    });
-
-    console.log(result.data);
-    //   console.log("MAX-UV :",result.uv_max);
-    //   console.log("MAX-UV TIME :",result.uv_max_time);
-    res.render("index.ejs", {
-      content: result.data,
-      Time: IstTime,
-      advice,
-      ActualCityName,
-      ActualCountryName,
-      ActualStateName,
-      ActualCityDistrictName,
-      ActualContinent,
-      //this error have to be null otherwise it will remains old values
-      error: null,
-    });
   } else {
-    res.render("index.ejs", {
-      error: "This location's data is not available...",
-      //this content have to be null otherwise it will remains old values
-      content: null,
-    });
-  }
-  }else{
     res.render("index.ejs", {
       error: "This location's data is not available...",
       //this content have to be null otherwise it will remains old values
--- sciptBtn.js
+++
@@ -1,8 +0,0 @@
-function CheckInput() {
-  var SubmitBtn = document.querySelector("#Btn");
-  var inputBox = document.querySelector("#areaName").value;
-
-  SubmitBtn.addEventListener("click", function () {
-    
-  });
-}
